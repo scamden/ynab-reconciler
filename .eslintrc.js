3d9160804734ab710c8589f32b56fe96560f172d
Eckhart-csjc <eckhart.csjc@gmail.com>
2021-01-22 13:55:18 -0500

    fix(eslint): add no-floating-promises rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -124,6 +124,7 @@ module.exports = {
     "@typescript-eslint/no-empty-function": "warn",
     "@typescript-eslint/no-empty-interface": "off",
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-floating-promises": "warn",
     "@typescript-eslint/no-inferrable-types": "off",
     "@typescript-eslint/no-misused-new": "warn",
     "@typescript-eslint/no-namespace": "warn",
